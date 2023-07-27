056e2caa4d41a502bd1fefa37e16335c5016bd3d
alee-anwar <me.malwar@gmail.com>
2023-07-28 03:41:54 +0500

    update

--- scripts/product-detail-page.js
+++ scripts/product-detail-page.js
@@ -1,7 +1,27 @@
-const slideshowNavImages = document.querySelectorAll(".Product__SlideshowNavImage");
+const slideshowNavImages = document.querySelectorAll(
+  ".Product__SlideshowNavImage"
+);
 
-slideshowNavImages.forEach(function(image) {
-  image.addEventListener("click", function() {
+slideshowNavImages.forEach(function (image) {
+  image.addEventListener("click", function () {
     this.classList.toggle("is-selected");
   });
+});
+
+function handleFlickityAttribute() {
+  const mainSlideshow = document.getElementById("mainSlideshow");
+
+  if (window.innerWidth <= 1140) {
+    mainSlideshow.setAttribute("data-flickity", "");
+  } else {
+    mainSlideshow.removeAttribute("data-flickity");
+  }
+}
+
+// Initial check on page load
+handleFlickityAttribute();
+
+// Listen for window resize and update the data-flickity attribute accordingly
+window.addEventListener("resize", function () {
+  handleFlickityAttribute();
 });
